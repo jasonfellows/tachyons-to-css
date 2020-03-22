befede05f7afcbb2073f2d548f0aeff0f0b11fe1
Jason Fellows <jasonfellows@gmail.com>
2020-03-22 16:05:31 +0100

    Updates

---
+++ precache-manifest.3e509e637d200cc824da02bb74ac25c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3350fa30764474dffc583ebabca2351d",
+    "url": "/tachyons-to-css/index.html"
+  },
+  {
+    "revision": "250f4c218f2314d4d96e",
+    "url": "/tachyons-to-css/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0410c36f5778c90f5c50",
+    "url": "/tachyons-to-css/static/js/2.9278af96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tachyons-to-css/static/js/2.9278af96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "250f4c218f2314d4d96e",
+    "url": "/tachyons-to-css/static/js/main.3972c51d.chunk.js"
+  },
+  {
+    "revision": "c974c4e00a2e1ba291de",
+    "url": "/tachyons-to-css/static/js/runtime-main.067152c2.js"
+  }
+]);
